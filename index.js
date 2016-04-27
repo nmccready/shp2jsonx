0af593ba25204a3259f873b5087d132ae3a4a844
nmccready <nemtcan@gmail.com>
2016-04-27 10:16:05 -0400

    indenting to 2 spaces

--- index.js
+++ index.js
@@ -14,207 +14,207 @@ var negativeFileRegExes = [];
 var alwaysReturnArray = null;
 
 var _parseOptions = function(opts) {
-    if (opts && typeof(opts) === 'object') {
-        if (opts.hasOwnProperty('shpFileFromArchive') && typeof(opts.shpFileFromArchive) === 'string')
-            shpFileFromArchive = opts.shpFileFromArchive;
-        if (opts.hasOwnProperty('xList')) {
-            if (typeof(opts.xList) === 'string')
-                xList = opts.xList.replace(/,/g);
-            if (Array.isArray(opts.xList))
-                xList = opts.xList.join(' ');
-        }
-        if (opts.hasOwnProperty('ignoreProperties')) {
-            if (typeof(opts.ignoreProperties) === 'string')
-                shapefileOpts['ignore-properties'] = opts.ignoreProperties === 'true';
-            if (typeof(opts.ignoreProperties) === 'boolean')
-                shapefileOpts['ignore-properties'] = opts.ignoreProperties;
-        }
-        if (opts.hasOwnProperty('xList')) {
-            if (typeof(opts.encoding) === 'string')
-                shapefileOpts.encoding = opts.encoding;
-        }
-        if (opts.negativeFileRegExes) {
-            negativeFileRegExes = opts.negativeFileRegExes;
-        }
-        if (opts.alwaysReturnArray !== null && typeof opts.alwaysReturnArray !== "undefined")
-            alwaysReturnArray = opts.alwaysReturnArray;
+  if (opts && typeof(opts) === 'object') {
+    if (opts.hasOwnProperty('shpFileFromArchive') && typeof(opts.shpFileFromArchive) === 'string')
+      shpFileFromArchive = opts.shpFileFromArchive;
+    if (opts.hasOwnProperty('xList')) {
+      if (typeof(opts.xList) === 'string')
+        xList = opts.xList.replace(/,/g);
+      if (Array.isArray(opts.xList))
+        xList = opts.xList.join(' ');
     }
+    if (opts.hasOwnProperty('ignoreProperties')) {
+      if (typeof(opts.ignoreProperties) === 'string')
+        shapefileOpts['ignore-properties'] = opts.ignoreProperties === 'true';
+      if (typeof(opts.ignoreProperties) === 'boolean')
+        shapefileOpts['ignore-properties'] = opts.ignoreProperties;
+    }
+    if (opts.hasOwnProperty('xList')) {
+      if (typeof(opts.encoding) === 'string')
+        shapefileOpts.encoding = opts.encoding;
+    }
+    if (opts.negativeFileRegExes) {
+      negativeFileRegExes = opts.negativeFileRegExes;
+    }
+    if (opts.alwaysReturnArray !== null && typeof opts.alwaysReturnArray !== "undefined")
+      alwaysReturnArray = opts.alwaysReturnArray;
+  }
 };
 
 module.exports = function(inStream, opts) {
-    var id = Math.floor(Math.random() * (1 << 30)).toString(16);
-    var tmpDir = path.join('/tmp', id);
-    var zipFile = path.join('/tmp', id + '.zip');
-    _parseOptions(opts);
-    if (shpFileFromArchive)
-        shpFileFromArchive = tmpDir + '/' + shpFileFromArchive;
+  var id = Math.floor(Math.random() * (1 << 30)).toString(16);
+  var tmpDir = path.join('/tmp', id);
+  var zipFile = path.join('/tmp', id + '.zip');
+  _parseOptions(opts);
+  if (shpFileFromArchive)
+    shpFileFromArchive = tmpDir + '/' + shpFileFromArchive;
 
-    var outStream = duplex.obj();
+  var outStream = duplex.obj();
 
-    var zipStream = fs.createWriteStream(zipFile);
-    inStream.pipe(zipStream);
-    zipStream.once('error', outStream.destroy);
+  var zipStream = fs.createWriteStream(zipFile);
+  inStream.pipe(zipStream);
+  zipStream.once('error', outStream.destroy);
 
-    seq()
-        .par(function() {
-            fs.mkdir(tmpDir, 0700, this);
-        })
-        .par(function() {
-            if (zipStream.closed) this();
-            else zipStream.once('close', this);
-        })
-        .seq_(function(next) {
-            // console.log(xList);
-            var ps = null;
-            if (!xList) {
-                ps = spawn('unzip', ['-d', tmpDir, zipFile]);
-            } else {
-                var toRun = 'unzip ' + zipFile + ' -d ' + tmpDir + ' -x ' + xList;
-                ps = exec(toRun);
+  seq()
+    .par(function() {
+      fs.mkdir(tmpDir, 0700, this);
+    })
+    .par(function() {
+      if (zipStream.closed) this();
+      else zipStream.once('close', this);
+    })
+    .seq_(function(next) {
+      // console.log(xList);
+      var ps = null;
+      if (!xList) {
+        ps = spawn('unzip', ['-d', tmpDir, zipFile]);
+      } else {
+        var toRun = 'unzip ' + zipFile + ' -d ' + tmpDir + ' -x ' + xList;
+        ps = exec(toRun);
+      }
+
+      ps.once('exit', function(code) {
+        next(code < 3 ? null : 'error in unzip: code ' + code)
+      });
+    })
+    .seq_(function(next) {
+      var s = findit(tmpDir);
+      var files = [];
+
+      var onFile = function(file) {
+        if (file.match(/__MACOSX/)) return;
+        if (file.match(/\.shp$|\.kml$/i)) {
+          var index;
+          var canPush = true;
+
+          for (index in negativeFileRegExes) {
+            if (file.match(negativeFileRegExes[index])) {
+              canPush = false;
+              break;
             }
+          }
+          if (canPush)
+            files.push(file);
+        }
+      };
 
-            ps.once('exit', function(code) {
-                next(code < 3 ? null : 'error in unzip: code ' + code)
-            });
-        })
-        .seq_(function(next) {
-            var s = findit(tmpDir);
-            var files = [];
+      s.on('file', onFile);
+      s.once('end', function() {
+        s.removeListener('file', onFile);
+        next.ok(files);
+      });
+    })
+    .seq(function(files) {
+      if (files.length === 0) {
+        this('no .shp files found in the archive');
+      } else if (shpFileFromArchive && files.indexOf(shpFileFromArchive) === -1) {
+        this('shpFileFromArchive: ' + shpFileFromArchive + 'does not exist in archive.');
+      } else {
+        if (shpFileFromArchive)
+          files = [shpFileFromArchive];
 
-            var onFile = function(file) {
-                if (file.match(/__MACOSX/)) return;
-                if (file.match(/\.shp$|\.kml$/i)) {
-                    var index;
-                    var canPush = true;
+        var maybeArrayBegining = '',
+          maybeArrayEnd = '',
+          maybeComma = '',
+          len = files.length,
+          after = '',
+          isFirstIteration = true,
+          i = 0,
+          resultIsArray = alwaysReturnArray !== null ? alwaysReturnArray : len > 1;
 
-                    for (index in negativeFileRegExes) {
-                        if (file.match(negativeFileRegExes[index])) {
-                            canPush = false;
-                            break;
-                        }
-                    }
-                    if (canPush)
-                        files.push(file);
+
+        var filePath, isLast, reader, fileName, before, started, currentLayer,
+          currentFeature, currentTransformation, firstTime, out;
+
+        function nextFile() {
+          if (i >= len) return;
+          filePath = files[i];
+          // console.log(i);
+          // console.log(filePath);
+          isLast = i === len - 1;
+          if (resultIsArray && i === 0) {
+            maybeArrayBegining = '[';
+            if (!isLast)
+              maybeComma = ',';
+          } else
+            maybeArrayBegining = '';
+
+          if (isLast && resultIsArray) {
+            maybeArrayEnd = ']';
+            maybeComma = '';
+          }
+          // console.log('reading next file: ' +  filePath);
+          reader = shp.reader(filePath, shapefileOpts);
+          fileName = filePath;
+          for (var toRemove in ['.shp', tmpDir])
+            fileName = filePath.replace(toRemove, '');
+
+          before = maybeArrayBegining + '{"type": "FeatureCollection","fileName": "' + fileName + '", "features": [\n';
+          after = '\n]}' + maybeComma + '\n' + maybeArrayEnd;
+          started = false;
+          firstTime = true;
+
+          out = '';
+        }
+        nextFile();
+
+        var layerStream = from(function(size, next) {
+
+          writeNextFeature();
+
+          function writeNextFeature() {
+
+            function readRecord() {
+              reader.readRecord(function(error, feature) {
+                if (feature == shp.end) {
+                  i++;
+                  layerStream.push(out);
+                  layerStream.push(after);
+                  reader.close();
+                  if (isLast) {
+                    // console.log('isLast');
+                    return layerStream.push(null);
+                  }
+                  nextFile();
+                  return writeNextFeature();
                 }
+                // if (!feature) return writeNextFeature();
+                // console.log(feature);
+                var featStr = JSON.stringify(feature);
+
+                if (started) {
+                  featStr = ',\n' + featStr;
+                } else {
+                  featStr = before + featStr;
+                }
+
+                started = true;
+                out += featStr;
+
+                if (out.length >= size) {
+                  next(null, out);
+                  out = '';
+                } else {
+                  writeNextFeature();
+                }
+              });
             };
-
-            s.on('file', onFile);
-            s.once('end', function() {
-                s.removeListener('file', onFile);
-                next.ok(files);
-            });
-        })
-        .seq(function(files) {
-            if (files.length === 0) {
-                this('no .shp files found in the archive');
-            } else if (shpFileFromArchive && files.indexOf(shpFileFromArchive) === -1) {
-                this('shpFileFromArchive: ' + shpFileFromArchive + 'does not exist in archive.');
-            } else {
-                if (shpFileFromArchive)
-                    files = [shpFileFromArchive];
-
-                var maybeArrayBegining = '',
-                    maybeArrayEnd = '',
-                    maybeComma = '',
-                    len = files.length,
-                    after = '',
-                    isFirstIteration = true,
-                    i = 0,
-                    resultIsArray = alwaysReturnArray !== null ? alwaysReturnArray : len > 1;
-
-
-                var filePath, isLast, reader, fileName, before, started, currentLayer,
-                    currentFeature, currentTransformation, firstTime, out;
-
-                function nextFile() {
-                    if (i >= len) return;
-                    filePath = files[i];
-                    // console.log(i);
-                    // console.log(filePath);
-                    isLast = i === len - 1;
-                    if (resultIsArray && i === 0) {
-                        maybeArrayBegining = '[';
-                        if (!isLast)
-                            maybeComma = ',';
-                    } else
-                        maybeArrayBegining = '';
-
-                    if (isLast && resultIsArray) {
-                        maybeArrayEnd = ']';
-                        maybeComma = '';
-                    }
-                    // console.log('reading next file: ' +  filePath);
-                    reader = shp.reader(filePath, shapefileOpts);
-                    fileName = filePath;
-                    for (var toRemove in ['.shp', tmpDir])
-                        fileName = filePath.replace(toRemove, '');
-
-                    before = maybeArrayBegining + '{"type": "FeatureCollection","fileName": "' + fileName + '", "features": [\n';
-                    after = '\n]}' + maybeComma + '\n' + maybeArrayEnd;
-                    started = false;
-                    firstTime = true;
-
-                    out = '';
-                }
-                nextFile();
-
-                var layerStream = from(function(size, next) {
-
-                    writeNextFeature();
-
-                    function writeNextFeature() {
-
-                        function readRecord() {
-                            reader.readRecord(function(error, feature) {
-                                if (feature == shp.end) {
-                                    i++;
-                                    layerStream.push(out);
-                                    layerStream.push(after);
-                                    reader.close();
-                                    if (isLast) {
-                                        // console.log('isLast');
-                                        return layerStream.push(null);
-                                    }
-                                    nextFile();
-                                    return writeNextFeature();
-                                }
-                                // if (!feature) return writeNextFeature();
-                                // console.log(feature);
-                                var featStr = JSON.stringify(feature);
-
-                                if (started) {
-                                    featStr = ',\n' + featStr;
-                                } else {
-                                    featStr = before + featStr;
-                                }
-
-                                started = true;
-                                out += featStr;
-
-                                if (out.length >= size) {
-                                    next(null, out);
-                                    out = '';
-                                } else {
-                                    writeNextFeature();
-                                }
-                            });
-                        };
-                        if (firstTime) {
-                            firstTime = false;
-                            reader.readHeader(function() {
-                                readRecord();
-                            });
-                        } else readRecord();
-                    }
-                });
-
-                outStream.setReadable(layerStream);
-                outStream.end(after);
-            }
-        })
-        .catch(function(err) {
-            outStream.destroy(err);
+            if (firstTime) {
+              firstTime = false;
+              reader.readHeader(function() {
+                readRecord();
+              });
+            } else readRecord();
+          }
         });
-    // return;
-    return outStream;
+
+        outStream.setReadable(layerStream);
+        outStream.end(after);
+      }
+    })
+    .catch(function(err) {
+      outStream.destroy(err);
+    });
+  // return;
+  return outStream;
 };
--- test/tojson.js
+++ test/tojson.js
@@ -6,127 +6,127 @@ var assertgeojson = require('geojson-assert');
 var now = new Date();
 
 [
-    {name:'shape', len: 162},
-    {name:'counties', len: 14},
-    {name:'senate', len: 40},
-].forEach(function(o){
-    test(o.name + '.zip to json', function (t) {
-        var inStream = fs.createReadStream(__dirname + '/../data/' + o.name + '.zip');
-        var outStream = new Stream;
-        outStream.writable = true;
-
-        var data = '';
-        outStream.write = function (buf) {
-            data += buf;
-        };
-
-        outStream.end = function () {
-            var geo = JSON.parse(data);
-            t.equal(typeof geo, 'object', 'is object');
-            t.equal(geo.features.length, o.len, 'got '+ o.len+' features');
-            t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
-            t.end();
-        };
-
-        toJSON(inStream).pipe(outStream);
-    });
-});
-
-
-test('combined.zip to json', function (t) {
-    var inStream = fs.createReadStream(__dirname + '/../data/combined.zip');
+  { name: 'shape', len: 162 },
+  { name: 'counties', len: 14 },
+  { name: 'senate', len: 40 },
+].forEach(function(o) {
+  test(o.name + '.zip to json', function(t) {
+    var inStream = fs.createReadStream(__dirname + '/../data/' + o.name + '.zip');
     var outStream = new Stream;
     outStream.writable = true;
 
     var data = '';
-    outStream.write = function (buf) {
-        data += buf;
+    outStream.write = function(buf) {
+      data += buf;
     };
 
-    outStream.end = function () {
-        var geos = JSON.parse(data);
-        t.equal(typeof geos, 'object', 'is object');
-        t.equal(Array.isArray(geos), true, 'is array');
-
-        t.equal(geos.length, 2, 'got 2 features');
-
-        var expected = [14,40];
-        for(var i; i< geos.length; i++){
-            var geo = geos[i];
-            t.equal(geo.features.length, expected[i], 'got '+ expected[i] +' features');
-            t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
-        }
-        t.end();
+    outStream.end = function() {
+      var geo = JSON.parse(data);
+      t.equal(typeof geo, 'object', 'is object');
+      t.equal(geo.features.length, o.len, 'got ' + o.len + ' features');
+      t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
+      t.end();
     };
 
-    toJSON(inStream).pipe(outStream)
+    toJSON(inStream).pipe(outStream);
+  });
 });
 
 
-test('negativeFileRegExes - combined.zip to json', function (t) {
-    var inStream = fs.createReadStream(__dirname + '/../data/combined.zip');
-    var outStream = new Stream;
-    outStream.writable = true;
+test('combined.zip to json', function(t) {
+  var inStream = fs.createReadStream(__dirname + '/../data/combined.zip');
+  var outStream = new Stream;
+  outStream.writable = true;
 
-    var data = '';
-    outStream.write = function (buf) {
-        data += buf;
-    };
+  var data = '';
+  outStream.write = function(buf) {
+    data += buf;
+  };
 
-    outStream.end = function () {
-        var geos = [JSON.parse(data)];
+  outStream.end = function() {
+    var geos = JSON.parse(data);
+    t.equal(typeof geos, 'object', 'is object');
+    t.equal(Array.isArray(geos), true, 'is array');
 
-        t.equal(typeof geos, 'object', 'is object');
-        t.equal(Array.isArray(geos), true, 'is array');
+    t.equal(geos.length, 2, 'got 2 features');
 
-        t.equal(geos.length, 1, 'got 1 features');
+    var expected = [14, 40];
+    for (var i; i < geos.length; i++) {
+      var geo = geos[i];
+      t.equal(geo.features.length, expected[i], 'got ' + expected[i] + ' features');
+      t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
+    }
+    t.end();
+  };
 
-        var expected = [40];
-        for(var i; i< geos.length; i++){
-            var geo = geos[i];
-            t.equal(geo.features.length, expected[i], 'got '+ expected[i] +' features');
-            t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
-        }
-        t.end();
-    };
-
-    toJSON(inStream, {negativeFileRegExes: [/counties/i]}).pipe(outStream)
-});
-
-test('alwaysReturnArray && negativeFileRegExes - combined.zip to json', function (t) {
-    var inStream = fs.createReadStream(__dirname + '/../data/combined.zip');
-    var outStream = new Stream;
-    outStream.writable = true;
-
-    var data = '';
-    outStream.write = function (buf) {
-        data += buf;
-    };
-
-    outStream.end = function () {
-        var geos = JSON.parse(data);
-        t.equal(typeof geos, 'object', 'is object');
-        t.equal(Array.isArray(geos), true, 'is array');
-
-        t.equal(geos.length, 1, 'got 1 features');
-
-        var expected = [40];
-        for(var i; i< geos.length; i++){
-            var geo = geos[i];
-            t.equal(geo.features.length, expected[i], 'got '+ expected[i] +' features');
-            t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
-        }
-        t.end();
-    };
-
-    toJSON(inStream, {
-      alwaysReturnArray: true,
-      negativeFileRegExes: [/counties/i]
-    }).pipe(outStream)
+  toJSON(inStream).pipe(outStream)
 });
 
 
-test("end", function(t){
+test('negativeFileRegExes - combined.zip to json', function(t) {
+  var inStream = fs.createReadStream(__dirname + '/../data/combined.zip');
+  var outStream = new Stream;
+  outStream.writable = true;
+
+  var data = '';
+  outStream.write = function(buf) {
+    data += buf;
+  };
+
+  outStream.end = function() {
+    var geos = [JSON.parse(data)];
+
+    t.equal(typeof geos, 'object', 'is object');
+    t.equal(Array.isArray(geos), true, 'is array');
+
+    t.equal(geos.length, 1, 'got 1 features');
+
+    var expected = [40];
+    for (var i; i < geos.length; i++) {
+      var geo = geos[i];
+      t.equal(geo.features.length, expected[i], 'got ' + expected[i] + ' features');
+      t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
+    }
+    t.end();
+  };
+
+  toJSON(inStream, { negativeFileRegExes: [/counties/i] }).pipe(outStream)
+});
+
+test('alwaysReturnArray && negativeFileRegExes - combined.zip to json', function(t) {
+  var inStream = fs.createReadStream(__dirname + '/../data/combined.zip');
+  var outStream = new Stream;
+  outStream.writable = true;
+
+  var data = '';
+  outStream.write = function(buf) {
+    data += buf;
+  };
+
+  outStream.end = function() {
+    var geos = JSON.parse(data);
+    t.equal(typeof geos, 'object', 'is object');
+    t.equal(Array.isArray(geos), true, 'is array');
+
+    t.equal(geos.length, 1, 'got 1 features');
+
+    var expected = [40];
+    for (var i; i < geos.length; i++) {
+      var geo = geos[i];
+      t.equal(geo.features.length, expected[i], 'got ' + expected[i] + ' features');
+      t.doesNotThrow(function() { assertgeojson(data); }, 'is valid geojson');
+    }
+    t.end();
+  };
+
+  toJSON(inStream, {
+    alwaysReturnArray: true,
+    negativeFileRegExes: [/counties/i]
+  }).pipe(outStream)
+});
+
+
+test("end", function(t) {
   console.log("total time: ");
   console.log(new Date() - now);
   t.end()
